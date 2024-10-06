822177c9e665d7dee6423fe93e3e9fdeddf1a468
M Waleed Kadous <waleedk@gmail.com>
2024-10-06 09:43:26 -0700

    Finish fixing.

--- buildprod.sh
+++ buildprod.sh
@@ -4,4 +4,3 @@ rm -rf yarn.lock node_modules build package-lock.json
 yarn install 
 yarn add --dev @typescript-eslint/eslint-plugin @typescript-eslint/parser prettier eslint-config-prettier eslint-plugin-prettier
 yarn build
-mkdocs build -c -f docs/mkdocs.yml -d $PWD/build/docs

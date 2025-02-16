a50e0ba56e8242819d7a5b20c6c20bf3189cabac
Amr Elsayed <amrmelsayed@users.noreply.github.com>
2025-02-17 10:42:47 +1100

    Configure React Native Reanimated

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,8 @@
 module.exports = function (api) {
   api.cache(true)
-  const plugins = []
-
   return {
     presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
 
-    plugins,
+    plugins: ['@babel/plugin-proposal-export-namespace-from', 'react-native-reanimated/plugin'],
   }
 }
--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,9 @@
 const { getDefaultConfig } = require('expo/metro-config')
 const { withNativeWind } = require('nativewind/metro')
+const { wrapWithReanimatedMetroConfig } = require('react-native-reanimated/metro-config')
 
 const config = getDefaultConfig(__dirname)
 
-module.exports = withNativeWind(config, { input: './src/global.css' })
+const nativeWindConfig = withNativeWind(config, { input: './src/global.css' })
+
+module.exports = wrapWithReanimatedMetroConfig(nativeWindConfig)
